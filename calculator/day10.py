a1f9ef77e2e4f7d6ecf94604af2c33410a7f0fb7
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-18 07:42:00 -0400

    start calculator project

---
+++ calculator/day10.py
@@ -0,0 +1,14 @@
+""" Functions with Outputs """
+
+# def example_output():
+#     results = 3 * 2
+#     return results
+
+# def format_name(first_name, last_name):
+#     full_name = f"{first_name} {last_name}".title()
+#     return full_name
+
+# print(format_name("jerome", "haynes"))
+
+from art import logo
+print(logo)
